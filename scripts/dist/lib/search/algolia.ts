a6d5464124b5aca7e93eb5a00a1afb5d4e63b09d
zoobestik <kb.chernenko@gmail.com>
2026-05-04 12:08:08 +0200

    chore(search): add submission stats for Algolia index updates

--- scripts/dist/lib/search/algolia.ts
+++ scripts/dist/lib/search/algolia.ts
@@ -3,11 +3,16 @@ import { env } from 'node:process';
 import { algoliasearch } from 'algoliasearch';
 
 export async function writeAlgoliaIndex(objects: Record<string, unknown>[]) {
-    await algoliasearch(process.env['WH_SEARCH_USER'], process.env['WH_SEARCH_WRITE_KEY'])
+    const result = await algoliasearch(process.env['WH_SEARCH_USER'], process.env['WH_SEARCH_WRITE_KEY'])
         .replaceAllObjects({
             indexName: process.env['ALGOLIA_INDEX_NAME'],
             objects
         });
 
     console.log(`Submitting Algolia index objects to ${env['ALGOLIA_INDEX_NAME']} index`);
+    console.log(`Submission stats: ${JSON.stringify({
+        copy: result.copyOperationResponse,
+        move: result.moveOperationResponse,
+        batch: result.batchResponses.length
+    })}`);
 }

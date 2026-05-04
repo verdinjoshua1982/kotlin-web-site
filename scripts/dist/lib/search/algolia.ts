928928436c1feb10d59884fa5988e04a0f7db6e5
zoobestik <kb.chernenko@gmail.com>
2026-05-04 15:02:21 +0200

    feat(search): save Algolia submission result to a JSON report

--- scripts/dist/lib/search/algolia.ts
+++ scripts/dist/lib/search/algolia.ts
@@ -1,6 +1,9 @@
 import { env } from 'node:process';
 
 import { algoliasearch } from 'algoliasearch';
+import { join } from 'node:path';
+import { REPORT_FOLDER } from '../files/index.js';
+import { writeFile } from 'node:fs/promises';
 
 export async function writeAlgoliaIndex(objects: Record<string, unknown>[]) {
     const result = await algoliasearch(process.env['WH_SEARCH_USER'], process.env['WH_SEARCH_WRITE_KEY'])
@@ -10,9 +13,6 @@ export async function writeAlgoliaIndex(objects: Record<string, unknown>[]) {
         });
 
     console.log(`Submitting Algolia index objects to ${env['ALGOLIA_INDEX_NAME']} index`);
-    console.log(`Submission stats: ${JSON.stringify({
-        copy: result.copyOperationResponse,
-        move: result.moveOperationResponse,
-        batch: result.batchResponses.length
-    })}`);
+
+    await writeFile(join(REPORT_FOLDER, 'submitting-result.json'), JSON.stringify(result, null, 2));
 }
